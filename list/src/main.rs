2b0843a2eda41f53f260b1cb7a4308cfb1200716
buchiyexiao <1091404874@qq.com>
2020-01-13 21:29:28 +0800

    new

--- list/src/main.rs
+++ list/src/main.rs
@@ -6,12 +6,15 @@ enum List{
 }
 
 impl List{
+    //定义一个空的List链表
     fn new()->List{
         Nil
     }
+    //向一个链表中插入一个elem 返回新链表 因此用self即可
     fn prepend(self,elem:i64) -> List{
         Cons(elem,Box::new(self))
     }
+    //类似于递归进行长度的计算
     fn len(&self) -> u32{
         match *self {
             Cons(_,ref tail) => 1 + tail.len(),
@@ -20,6 +23,7 @@ impl List{
             Nil => 0
         }
     }
+    //将一个链表拼接成为一段String
     fn strintify(&self) -> String{
         match *self{
             Cons(head,ref tail) => {
